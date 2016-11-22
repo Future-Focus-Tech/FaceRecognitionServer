27c11e5254f803646dbc81c4146280511a748c13
rajur7 <rrr537@gmail.com>
2016-11-22 12:53:30 +0530

    [Joseph/Raju/JP]  Using dependencies through gradle instead of manually adding.

--- src/main/java/com/mkyong/helloworld/web/WelcomeController.java
+++ src/main/java/com/mkyong/helloworld/web/WelcomeController.java
@@ -4,68 +4,82 @@ import com.mkyong.helloworld.service.HelloWorldService;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.MatVector;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.imageio.ImageIO;
 import javax.xml.bind.DatatypeConverter;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
+import static org.springframework.http.ResponseEntity.ok;
 
 
 @Controller
 public class WelcomeController {
 
-	private final HelloWorldService helloWorldService;
+    private final HelloWorldService helloWorldService;
 
-	@Autowired
-	public WelcomeController(HelloWorldService helloWorldService) {
-		this.helloWorldService = helloWorldService;
-	}
+    @Autowired
+    public WelcomeController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Map<String, Object> model) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Map<String, Object> model) {
 
-		model.put("title", helloWorldService.getTitle(""));
-		model.put("msg", helloWorldService.getDesc());
-		
-		return "index";
-	}
+        model.put("title", helloWorldService.getTitle(""));
+        model.put("msg", helloWorldService.getDesc());
 
-	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
-	public ModelAndView hello(@PathVariable("name") String name) {
-		
-		ModelAndView model = new ModelAndView();
-		model.setViewName("index");
-		
-		model.addObject("title", helloWorldService.getTitle(name));
-		model.addObject("msg", helloWorldService.getDesc());
-		
-		return model;
+        return "index";
+    }
 
-	}
-	@RequestMapping(value = "face", method = RequestMethod.POST)
-	public ResponseEntity<MatVector>  faces(@RequestParam(value = "facesData[]") String[] facesData) throws IOException {
-		System.out.println(facesData.length);
-		String encodingPrefix = "base64,";
-		String dataUrl = facesData[1];
-		int contentStartIndex = dataUrl.indexOf(encodingPrefix) + encodingPrefix.length();
-		byte[] imageData = DatatypeConverter.parseBase64Binary(dataUrl.substring(contentStartIndex));
-		System.out.println(imageData.length);
+    @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
 
-		Mat mat = new Mat();
-		mat.data().put(imageData);
+        ModelAndView model = new ModelAndView();
+        model.setViewName("index");
 
+        model.addObject("title", helloWorldService.getTitle(name));
+        model.addObject("msg", helloWorldService.getDesc());
 
-//        InputStream inputStream = new ByteArrayInputStream(imageData);
-//        BufferedImage bufferedImage = ImageIO.read(inputStream);
-//        ImageIO.write(bufferedImage, "png", new File("image.png"));
-		return new ResponseEntity<MatVector>(HttpStatus.OK);
-	}
+        return model;
+
+    }
+
+    @RequestMapping(value = "/face", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> faces(@RequestParam(value = "facesData[]") String[] facesData) throws IOException {
+        int numberOfFaces = facesData.length;
+        MatVector faces = new MatVector(numberOfFaces);
+
+        for (int faceIndex = 0; faceIndex < numberOfFaces; faceIndex++) {
+            Mat matImage = createFaceMatrix(facesData[faceIndex]);
+			imwrite("image" +faceIndex+".png", matImage);
+            faces.put(faceIndex, matImage);
+        }
+
+        return ok("thank you");
+    }
+
+    private Mat createFaceMatrix(String imageDataUrl) throws IOException {
+        String encodingPrefix = "base64,";
+        int contentStartIndex = imageDataUrl.indexOf(encodingPrefix) + encodingPrefix.length();
+        byte[] imageData = DatatypeConverter.parseBase64Binary(imageDataUrl.substring(contentStartIndex));
+        InputStream inputStream = new ByteArrayInputStream(imageData);
+        BufferedImage bufferedImage = ImageIO.read(inputStream);
+        ImageIO.write(bufferedImage,"png", new File("image.png"));
+        return new Mat(imread("image.png"));
+//        OpenCVFrameConverter.ToMat cv = new OpenCVFrameConverter.ToMat();
+//        return cv.convertToMat(new Java2DFrameConverter().convert(bufferedImage));
+    }
 
 }
